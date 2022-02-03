36c258ee10b5cee89d7d6417390e1c0a1a4785c8
Audie Moradi <audiemoradi@gmail.com>
2022-02-03 16:14:15 -0600

    Changed some of the initial middleware - per the docs

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-//in index.js
+// First, we declare the middleware we will use
+const config = require('config')
+const path = require('path')
 const Koa = require('koa')
-const app = new Koa()
+const Router = require('koa-router')
+const loadRoutes = require("./app/routes")
+const DataLoader = require('./app/dataLoader')
 const views = require('koa-views')
 const serve = require('koa-static')
-const path = require('path')
-const config = require('config') // file where we will add all of our app's configuration
+const app = new Koa()
+const router = new Router()
 // Views setup, adds render() function to ctx object
 app.use(views(
   path.join(__dirname, config.get('views.path')),
