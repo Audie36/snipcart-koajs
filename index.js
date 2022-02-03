8070ffc348d4a50676e073dfc80e51839f187228
Audie Moradi <audiemoradi@gmail.com>
2022-02-02 20:39:17 -0600

    Added index.js file.

---
+++ index.js
@@ -0,0 +1,12 @@
+//in index.js
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const serve = require('koa-static')
+const path = require('path')
+const config = require('config') // file where we will add all of our app's configuration
+// Views setup, adds render() function to ctx object
+app.use(views(
+  path.join(__dirname, config.get('views.path')),
+  config.get('views.options')
+))
