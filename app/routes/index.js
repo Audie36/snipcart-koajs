1b3a66adf87e2e92bef39cc1527bd5bcc29662b5
Audie Moradi <audiemoradi@gmail.com>
2022-10-20 08:09:54 -0500

    Remove duplicate require

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,5 @@
 require("newrelic");
 
-require("newrelic");
-
 module.exports = (router, productsLoader) => {
     require('./home')(router, productsLoader)
     require('./buy')(router, productsLoader)
