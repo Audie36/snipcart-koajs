ee7ee144ba83469a2419890bfe4cef03f8f1d126
Audie Moradi <audiemoradi@gmail.com>
2023-05-26 09:57:40 -0500

    Added user identification

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
 require("newrelic");
 
+newrelic.addCustomAttribute("enduser.id", "audiemoradi@msn.com");
+
 module.exports = (router, productsLoader) => {
     require('./home')(router, productsLoader)
     require('./buy')(router, productsLoader)
