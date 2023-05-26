56e74bbf763654438e2f8d69e8eaa8cce89312df
Audie Moradi <audiemoradi@gmail.com>
2023-05-26 10:20:45 -0500

    Testing User instrumentation

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,8 @@
 module.exports = (router, productsLoader) => {
     router.get('/', async ctx => {
+      // Add in user instrumentation
+      newrelic.addCustomAttribute("enduser.id", "audiemoradi@msn.com")
+      
       const products = await productsLoader.all()
       ctx.state.model = {
         title: 'Hey there,',
