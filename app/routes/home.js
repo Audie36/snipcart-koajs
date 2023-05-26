f36f492e2bd698a4fc2ac318b5f484889b808fb6
Audie Moradi <audiemoradi@gmail.com>
2023-05-26 10:22:17 -0500

    Removed User Instrumentation

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,8 +1,5 @@
 module.exports = (router, productsLoader) => {
     router.get('/', async ctx => {
-      // Add in user instrumentation
-      newrelic.addCustomAttribute("enduser.id", "audiemoradi@msn.com")
-      
       const products = await productsLoader.all()
       ctx.state.model = {
         title: 'Hey there,',
